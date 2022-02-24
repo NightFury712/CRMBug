edc3aa0f743ef749add6f592a2ee15498828e281
phmanh macos <phimanh2905@gmail.com>
2022-02-24 08:46:19 +0700

    ghép fe - be

--- CRMBug-FE/src/app/service/issue.service.ts
+++ CRMBug-FE/src/app/service/issue.service.ts
@@ -3,11 +3,11 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class IssueService {
-  url: string = 'https://localhost:44378/api/v1/Issue';
-  constructor(private http: HttpClient) { }
+  url: string = 'https://localhost:5001/api/v1/Issue';
+  constructor(private http: HttpClient) {}
 
   getIssues(): Observable<any> {
     return this.http.get<any>(this.url);
