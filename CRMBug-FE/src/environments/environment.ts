38c94dea80e8294cfeff224700ba22b39706d017
phmanh_msi <phimanh2905@gmail.com>
2022-03-01 10:41:19 +0700

    thêm ignore file

--- CRMBug-FE/src/environments/environment.ts
+++ CRMBug-FE/src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  // endPoint: "https://localhost:5001/api",
+  endPoint: 'https://localhost:44378/api/',
 };
 
 /*
