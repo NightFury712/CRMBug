4bd6beb1de58e5a2592a3a55ce6661bb2a6bfcf6
Dang <haidangk63@gmail.com>
2022-03-01 14:14:12 +0700

    Update 2 3.1.2022

--- CRMBug-BE/ApplicationCore/BL/BLEmployee.cs
+++ CRMBug-BE/ApplicationCore/BL/BLEmployee.cs
@@ -5,11 +5,21 @@ using System.Text;
 using System.Threading.Tasks;
 using ApplicationCore.Entities;
 using ApplicationCore.Interfaces.BL;
+using ApplicationCore.Interfaces.DL;
 
 namespace ApplicationCore.BL
 {
   public class BLEmployee : BLBase<Employee>, IBLEmployee
   {
+    #region DECLARE
+    IDLEmployee DLEmployee;
+    #endregion
 
+    #region CONSTRUCTOR
+    public BLEmployee(IDLEmployee dlEmployee) : base(dlEmployee)
+    {
+      DLEmployee = dlEmployee;
+    }
+    #endregion
   }
 }
