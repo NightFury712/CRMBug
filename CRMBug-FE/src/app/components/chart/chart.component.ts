ef2df38a8acf47ed692f2bf1472aaf960faef1ea
Dang <haidangk63@gmail.com>
2022-03-06 17:32:00 +0700

    Update bug tracking 6/3/2022

--- CRMBug-FE/src/app/components/chart/chart.component.ts
+++ CRMBug-FE/src/app/components/chart/chart.component.ts
@@ -6,7 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./chart.component.scss']
 })
 export class ChartComponent implements OnInit {
-
+  priorities = [
+    {
+      value: '1',
+      valueText: 'Hello'
+    },
+    {
+      value: '2',
+      valueText: 'Hi'
+    },
+    {
+      value: '3',
+      valueText: 'Good'
+    }
+  ]
   constructor() { }
 
   ngOnInit(): void {
