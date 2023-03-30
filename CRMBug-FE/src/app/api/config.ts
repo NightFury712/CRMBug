c2318413549749675792de431b913062f605f403
Dang <haidangk63@gmail.com>
2023-03-30 22:14:35 +0700

    Update

--- CRMBug-FE/src/app/api/config.ts
+++ CRMBug-FE/src/app/api/config.ts
@@ -1,5 +1,5 @@
 export const APIConfig = {
-  development: 'https://localhost:44378',
+  development: 'http://localhost:44378',
   production: 'https://localhost:44378',
   schedule: 'http://localhost:44341',
 }
